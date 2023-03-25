2d88c6e14ce4b5b995be0671b5c4843f9ab8292b
Janko <janko.jelic99@gmail.com>
2023-03-25 17:45:51 +0100

    initial folder structure with absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@containers': './src/containers',
+          '@constants': './src/constants',
+          '@hoc': './src/hoc',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@util': './src/util',
+        },
+      },
+    ],
+  ],
 };
