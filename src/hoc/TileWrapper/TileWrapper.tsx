4c32940e2099878f444c714258157f1ddcf62df2
Janko <janko.jelic99@gmail.com>
2023-05-14 00:59:52 +0200

    Update TileWrapper.tsx

--- src/hoc/TileWrapper/TileWrapper.tsx
+++ src/hoc/TileWrapper/TileWrapper.tsx
@@ -2,14 +2,37 @@ import { Colors } from 'constants/styles/Colors';
 import { AN, BORDER_RADIUS } from 'constants/styles/appStyles';
 import TouchableBounce from 'hoc/TouchableBounce';
 import useStyles from 'hooks/styles/useStyles';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
+import { setColorOpacity } from 'util/strings/setColorOpacity';
 
 const TileWrapper = ({ children, style = {}, onPress = () => {} }: Props) => {
-  const { styles } = useStyles(createStyles);
+  const { styles, colors } = useStyles(createStyles);
+  const [backgroundOpacity, setBackgroundOpacity] = useState(1);
+
+  const onPressIn = () => {
+    setBackgroundOpacity(0.7);
+  };
+
+  const onPressOut = () => {
+    setBackgroundOpacity(1);
+  };
 
   return (
-    <TouchableBounce onPress={onPress} style={[styles.container, style]}>
+    <TouchableBounce
+      onPress={onPress}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      style={[
+        styles.container,
+        style,
+        {
+          backgroundColor: setColorOpacity(
+            colors.tileBackground,
+            backgroundOpacity,
+          ),
+        },
+      ]}>
       {children}
     </TouchableBounce>
   );
