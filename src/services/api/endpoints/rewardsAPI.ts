f23031f8949b3a63882e5682e37a8a3eb62fb499
Janko <janko.jelic99@gmail.com>
2023-05-24 08:36:37 +0200

    Update rewardsAPI.ts

--- src/services/api/endpoints/rewardsAPI.ts
+++ src/services/api/endpoints/rewardsAPI.ts
@@ -4,7 +4,7 @@ const { get, post } = httpClient;
 
 const rewardsAPI = {
   registerArenaGameScore: async (score: Record<number, number>) => {
-    const { data } = await post('/rewards/arena/score', score);
+    const { data } = await post<number>('/rewards/arena/score', score);
     return data;
   },
 };
