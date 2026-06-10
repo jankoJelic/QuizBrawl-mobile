1434ff343e0ed3e0f68a193d346c5a5ac6f55f2b
jankojelickriptomat <115019619+jankojelickriptomat@users.noreply.github.com>
2026-06-10 12:40:43 +0200

    ..

--- app/screens/RoomScreen/RoomScreen.tsx
+++ app/screens/RoomScreen/RoomScreen.tsx
@@ -1,45 +1,49 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import CTA from 'components/buttons/CTA';
-import GhostButton from 'components/buttons/GhostButton/GhostButton';
-import RoundButton from 'components/buttons/RoundButton/RoundButton';
-import VolumeIcon from 'components/icons/VolumeIcon/VolumeIcon';
-import NavHeader from 'components/layout/NavHeader';
-import InfoLine from 'components/tiles/InfoLine/InfoLine';
-import UserTile from 'components/tiles/UserTile/UserTile';
-import { AN } from 'constants/styles/appStyles';
-import UserActionSheet from 'containers/ActionSheet/UserActionSheet';
-import MyScrollView from 'hoc/MyScrollView';
-import ScreenWrapper from 'hoc/ScreenWrapper';
-import { MainStackParamsList } from 'navigation/MainStackParamsList';
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { SOCKET, SOCKET_EVENTS } from 'services/socket/socket';
-import { useAppSelector } from 'store/index';
-import { startLoading } from 'store/slices/appStateSlice';
-import { removeUserFromRoom } from 'store/slices/dataSlice';
-import { ShallowUser } from 'store/types/authSliceTypes';
-import { Room } from 'store/types/dataSliceTypes';
+import { capitalizeFirstLetter } from "@/app/util/strings/capitalizeFirstLetter";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import CTA from "components/buttons/CTA";
+import GhostButton from "components/buttons/GhostButton/GhostButton";
+import RoundButton from "components/buttons/RoundButton/RoundButton";
+import VolumeIcon from "components/icons/VolumeIcon/VolumeIcon";
+import NavHeader from "components/layout/NavHeader";
+import InfoLine from "components/tiles/InfoLine/InfoLine";
+import UserTile from "components/tiles/UserTile/UserTile";
+import { AN } from "constants/styles/appStyles";
+import UserActionSheet from "containers/ActionSheet/UserActionSheet";
+import MyScrollView from "hoc/MyScrollView";
+import ScreenWrapper from "hoc/ScreenWrapper";
+import { MainStackParamsList } from "navigation/MainStackParamsList";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { useDispatch } from "react-redux";
+import { SOCKET, SOCKET_EVENTS } from "services/socket/socket";
+import { useAppSelector } from "store/index";
+import { startLoading } from "store/slices/appStateSlice";
+import { removeUserFromRoom } from "store/slices/dataSlice";
+import { ShallowUser } from "store/types/authSliceTypes";
+import { Room } from "store/types/dataSliceTypes";
 
 const RoomScreen: React.FC<
-  NativeStackScreenProps<MainStackParamsList, 'Room'>
+  NativeStackScreenProps<MainStackParamsList, "Room">
 > = ({ navigation, route }) => {
   const dispatch = useDispatch();
-  const { onQuestion } = useAppSelector(state => state.game);
-  const { rooms, userData } = useAppSelector(state => state.data);
-  const room = rooms.find(r => r.id === route.params.room.id);
+  const { onQuestion } = useAppSelector((state) => state.game);
+  const { rooms, userData, topics } = useAppSelector((state) => state.data);
+  const room = rooms.find((r) => r.id === route.params.room.id);
   const {
     name,
     userId,
     maxPlayers,
     users,
     questionsCount,
-    topic,
+    topicId,
     answerTime,
     readyUsers,
     hostName,
   } = room || {};
   const isRoomAdmin = userId === userData.id;
+  const topic = capitalizeFirstLetter(
+    topics.find((t) => t.id === topicId)?.name || "",
+  );
 
   const [userActionSheetVisible, setUserActionSheetVisible] = useState(false);
   const [selectedUser, setSelectedUser] = useState<ShallowUser>();
@@ -48,11 +52,11 @@ const RoomScreen: React.FC<
 
   useEffect(() => {
     if (onQuestion === 0)
-      navigation.navigate('GameSplash', { room: room as Room });
+      navigation.navigate("GameSplash", { room: room as Room });
   }, [onQuestion]);
 
   const onPressLeftArrow = () => {
-    navigation.navigate('Lobby', { lobbyId: room?.lobbyId as number });
+    navigation.navigate("Lobby", { lobbyId: room?.lobbyId as number });
     SOCKET.emit(SOCKET_EVENTS.USER_LEFT_ROOM, { user: userData, room });
     dispatch(removeUserFromRoom({ room: room as Room, user: userData }));
   };
@@ -114,7 +118,7 @@ const RoomScreen: React.FC<
         <InfoLine title="Answer time:" value={`${answerTime} seconds`} />
         <InfoLine title="Number of questions:" value={String(questionsCount)} />
         <View style={{ marginVertical: AN(10) }}>
-          {users?.map(u => (
+          {users?.map((u) => (
             <UserTile
               key={`userInRoom-${u.id}`}
               user={u}
@@ -131,7 +135,7 @@ const RoomScreen: React.FC<
         {!youAreReady ? (
           <RoundButton
             title="Ready"
-            style={{ alignSelf: 'center', marginTop: AN(10) }}
+            style={{ alignSelf: "center", marginTop: AN(10) }}
             color="success500"
             size={AN(50)}
             onPress={onPressReady}
